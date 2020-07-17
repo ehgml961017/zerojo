7bc2698052dca1e5e08b0c072c93c78904ebafbf
dohee <ehgml961017@gmail.com>
2020-07-17 09:53:51 +0900

    - git spring boot project 생성 - git android project 생성

---
+++ zerojoproject/zerojoproject/src/main/java/com/example/zerojoproject/ZerojoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerojoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerojoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerojoprojectApplication.class, args);
+	}
+
+}
---
+++ zerojoproject/zerojoproject/src/test/java/com/example/zerojoproject/ZerojoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zerojoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerojoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
